1c9eef3fc79feb0d6d385ca43e0ae46139e03399
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2022-03-31 11:40:54 +0600

    Insert Date Using  Node JS with MongoDB

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 var mongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://nirobdb:nirobdb@cluster0.jw2lu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+var url = "mongodb+srv://nirobdb:nirobdb@cluster0.jw2lu.mongodb.net?retryWrites=true&w=majority";
 var config = {useUnifiedTopology: true};
 
-mongoClient.connect(url, config, function (error){
+mongoClient.connect(url, config, function (error, myMongoClient){
    if (error){
        console.log("Connection Fail");
    } else{
        console.log("Connection Success");
+       insertData(myMongoClient);
    }
 });
+
+function insertData(myMongoClient){
+    var catchDatabase = myMongoClient.db('school');
+    var catchTable =  catchDatabase.collection('students');
+
+    var data = {
+        name:"Nirob",
+        roll:"01",
+        class:"Ten",
+        city:"Dhaka"
+    }
+    catchTable.insertOne(data, function (error){
+        if (error){
+            console.log("Data Insert Fail");
+        } else{
+            console.log("Data Insert Success");
+        }
+    })
+}
